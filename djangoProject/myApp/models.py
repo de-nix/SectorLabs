a13785eea8cd1093a3ecb23e192bfd9f61f4e29a
Denis <criste.denis15@yahoo.com>
2021-10-16 14:27:54 +0300

    first functionality met - display the public gists of a user + model changes + parsing the json response into files and gists

--- djangoProject/myApp/models.py
+++ djangoProject/myApp/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class User:
     name= models.CharField(max_length=100)
     photo= models.ImageField(upload_to='pics')
 
+class File:
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+    filetype= models.CharField(max_length=100)
+
 class Gist:
     name = models.CharField(max_length=100)
-    filetype= models.CharField(max_length=100)
+    files = [File]
     forks= [User]
-    content = models.TextField()
--- djangoProject/myApp/views.py
+++ djangoProject/myApp/views.py
@@ -3,6 +3,8 @@ import time
 
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from . import models
 from .models import User
 import requests
 
@@ -16,7 +18,20 @@ def getHomeWithUser(request):
     if username != "":
         text = 'https://api.github.com/users/' + username + '/gists'
         r = json.loads(requests.get(text).text)
-        print(r)
+        for gist in r:
+            g = models.Gist()
+            g.files = []
+            g.forks = []
+            g.name = gist["description"]
+            for file in gist["files"]:
+                f = models.File()
+                f.name = gist["files"][file]["filename"]
+                f.filetype = gist["files"][file]["type"]
+                f.content = gist["files"][file]["raw_url"]
+                g.files.append(f)
+            gists.append(g)
+            print(g.name)
+
     return render(request, "home.html", {'gists': gists})
 
 def getHome(request):
